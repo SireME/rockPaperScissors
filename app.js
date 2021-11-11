efff4d5b2fded1b44632c05f7ff0119136233e1f
Mbang <mbangernest@yahoo.com>
2021-11-11 15:17:51 +0100

    function for random computer play

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-function testAlert(){
-    alert("hello world");
+function computerPlay(){
+    let computerVal=Math.floor(Math.random()*3);
+    if (computerVal===0){
+        return "rock"
+    }else if(computerVal===1){
+        return "paper"
+    }else if (computerVal===2){
+        return "scissors"
+    }
 }
-testAlert();
