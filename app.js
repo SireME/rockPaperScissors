4aedbb54452d10272fa1eec206e4c5f39930be98
Mbang <mbangernest@yahoo.com>
2021-11-19 18:27:05 +0100

    Basic RPS single play

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
+
+
 function computerPlay(){
     let computerChoice=Math.floor(Math.random()*3);
-    const compVal=["rock","paper","scissors"]
-    return compVal[computerChoice]
+    const compVal=["rock","paper","scissors"];
+    return compVal[computerChoice];
 }
-console.log(computerPlay())
+function playerSelecct(){
+    let plInput=prompt("Your Selection? \n rock paper or Scissors").toLowerCase();
+    return plInput
+}
+
+function playGame(){
+    const playerInput=playerSelecct()
+    const computerPl=computerPlay();
+    let winState="Wow You won"
+    if (playerInput==="rock" && computerPl==="scissors"){
+        winState;
+    }else if(playerInput==="paper" && computerPl==="rock"){
+        winState;
+    }else if(playerInput==="scissors" && computerPl==="paper"){
+        winState;
+    }else if(playerInput===computerPl){
+        winState="it's a tie";
+    }else{
+        winState="You loose";
+    }
+    console.log(`You played ${playerInput} and the computer played ${computerPl}  \n RESULTS`)
+    return winState
+ 
+}
+console.log(playGame())
+
